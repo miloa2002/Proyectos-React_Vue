04793ec8fa892502cfb0133655e1f2a961af04ec
miloa2002 <agudelo.tapias@gmail.com>
2024-10-18 13:11:22 -0500

    Feat: Dictionary-app Vue

---
+++ 01-Dictionary-app-vue/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import Result from "./components/Result.vue"
+import { ref } from "vue"
+import Alert from "./components/Alert.vue";
+
+const word = ref("")
+const data = ref({})
+const error = ref("")
+
+const fetchData = async () => {
+  if(word.value === "") {
+    error.value = "El campo no puede estar vacío"
+    return;
+  }
+
+  try {
+    const resposne = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word.value}`)
+    const result = await resposne.json()
+    data.value = result
+    
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <Alert v-if="error">
+      {{ error }}
+    </Alert>
+    <form @submit.prevent="fetchData" class="form">
+      <input v-model="word" type="text" class="search" placeholder="serene" />
+      <button class="search-button">Search</button>
+    </form>
+    <Result v-for="dictionary in data" :dictionary="dictionary" />
+  </div>
+</template>
---
+++ 01-Dictionary-app-vue/src/components/Result.vue
@@ -0,0 +1,18 @@
+<script setup>
+const data = defineProps({
+  dictionary: {
+    type: Object
+  }
+})
+
+console.log(data.dictionary);
+
+</script>
+
+<template>
+    <div class="result">
+      <h2 class="title-result">{{dictionary.word}}</h2>
+      <p v-for="antonyms in dictionary.meanings[2]?.antonyms" class="part-of-speech">antonyms: {{antonyms}}</p>
+      <p class="definition">{{dictionary.meanings[0].definitions[0].definition}}</p>
+    </div>
+</template>
